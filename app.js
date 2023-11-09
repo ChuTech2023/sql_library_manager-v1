1db9d6d25b183217f19a1cb7891e60519a9aca4c
ChuTech2023 <alice.ntam@gmail.com>
2023-11-09 14:10:32 -0800

    db sync

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const models = require('./models');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//db sync
+models.sequelize.sync()
+.then(() => models.sequelize.authenticate())
+.then(() => console.log("db connected"))
+.catch( (err) => console.log(err))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
