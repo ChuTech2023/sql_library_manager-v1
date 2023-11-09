9073a393c307c2730109ad683f3d081ef404a507
ChuTech2023 <alice.ntam@gmail.com>
2023-11-09 14:27:33 -0800

    home API route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const {Book} = require('../models')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  try {
+    const dbData = await Book.findAll()
+    console.log(dbData)
+    res.json(dbData)
+  } catch (error) {
+    
+  }
 });
 
 module.exports = router;
